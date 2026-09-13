ff9f54f611d3a70ff1eb8276bf69c1d8688f314b
Sheref17 <Sherefgamal17@gmail.com>
2026-09-13 10:35:09 +0300

    feat: add Redis caching for categories and brands

--- ECommerce.Application/Features/Brands/Queries/GetBrandById/GetBrandByIdQueryHandler.cs
+++ ECommerce.Application/Features/Brands/Queries/GetBrandById/GetBrandByIdQueryHandler.cs
@@ -1,10 +1,12 @@
 ﻿using ECommerce.Application.Abstractions.Repositories;
+using ECommerce.Application.Abstractions.Services;
 using ECommerce.Application.Features.Brands.Dtos;
 using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ECommerce.Application.Features.Brands.Queries.GetBrandById
@@ -12,16 +14,33 @@ namespace ECommerce.Application.Features.Brands.Queries.GetBrandById
     public class GetBrandByIdQueryHandler : IRequestHandler<GetBrandByIdQuery, BrandResponse?>
     {
         private readonly IBrandReadRepository _brandReadRepository;
+        private readonly ICacheService _cacheService;
 
-        public GetBrandByIdQueryHandler(IBrandReadRepository brandReadRepository)
+        public GetBrandByIdQueryHandler(IBrandReadRepository brandReadRepository ,
+            ICacheService cacheService)
         {
             _brandReadRepository = brandReadRepository;
+            _cacheService = cacheService;
         }
 
         public async Task<BrandResponse?> Handle( GetBrandByIdQuery request,
             CancellationToken cancellationToken)
         {
-            return await _brandReadRepository.GetByIdAsync(request.Id,cancellationToken);
+            var cacheKey = $"brand:{request.Id}";
+            var cachedBrand = await _cacheService.GetAsync(cacheKey , cancellationToken);
+            if (cachedBrand is not null)
+            {
+                return JsonSerializer.Deserialize<BrandResponse>(cachedBrand);
+            }
+            var brand = await _brandReadRepository.GetByIdAsync(request.Id,cancellationToken);
+            if (brand is null)
+            {
+                return null;
+            }
+            var serializedBrand = JsonSerializer.Serialize(brand);
+            await _cacheService.SetAsync(cacheKey,serializedBrand
+                ,TimeSpan.FromMinutes(10),cancellationToken);
+            return brand;
         }
     }
 }
--- ECommerce.Application/Features/Categories/Queries/GetCategoryById/GetCategoryByIdQueryHandler.cs
+++ ECommerce.Application/Features/Categories/Queries/GetCategoryById/GetCategoryByIdQueryHandler.cs
@@ -1,10 +1,12 @@
 ﻿using ECommerce.Application.Abstractions.Repositories;
+using ECommerce.Application.Abstractions.Services;
 using ECommerce.Application.Features.Categories.DTOs;
 using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ECommerce.Application.Features.Categories.Queries.GetCategoryById
@@ -12,16 +14,37 @@ namespace ECommerce.Application.Features.Categories.Queries.GetCategoryById
     public class GetCategoryByIdQueryHandler : IRequestHandler<GetCategoryByIdQuery, CategoryResponse?>
     {
         private readonly ICategoryReadRepository _categoryReadRepository;
+        private readonly ICacheService _cacheService;
 
-        public GetCategoryByIdQueryHandler(ICategoryReadRepository categoryReadRepository)
+        public GetCategoryByIdQueryHandler(ICategoryReadRepository categoryReadRepository,
+            ICacheService cacheService)
         {
             _categoryReadRepository = categoryReadRepository;
+            _cacheService = cacheService;
         }
 
         public async Task<CategoryResponse?> Handle(GetCategoryByIdQuery request,
             CancellationToken cancellationToken)
         {
-            return await _categoryReadRepository.GetByIdAsync(request.Id,cancellationToken);
+            var cacheKey = $"category:{request.Id}";
+
+            var cachedCategory = await _cacheService.GetAsync(cacheKey,cancellationToken);
+            if (cachedCategory is not null)
+            {
+                return JsonSerializer.Deserialize<CategoryResponse>(cachedCategory);
+            }
+           var category =await _categoryReadRepository
+                .GetByIdAsync(request.Id,cancellationToken);
+            if (category is null)
+            {
+                return null;
+            }
+            var serializedCategory = JsonSerializer.Serialize(category);
+
+            await _cacheService.SetAsync(cacheKey,serializedCategory,
+                TimeSpan.FromMinutes(10),cancellationToken);
+            return category;
+
         }
     }
 }
